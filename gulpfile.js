2b95fbb0e11abf1ac7857cc4a617edc1c8e44697
Eduard <kamaev-eduard@yandex.ru>
2022-12-20 15:08:52 +0500

    gitignore: - add folder "build"
    gulp.js:
    - добавил автоматизацию (не вся)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const htmlmin = require("gulp-htmlmin");
+const terser = require('gulp-terser');
+const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const webp = require("gulp-webp"); 
 
 // Styles
 
@@ -23,12 +28,89 @@ const styles = () => {
 
 exports.styles = styles;
 
+// HTML
+
+const html = () => {
+  return gulp.src("source/**/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("build"));
+}
+
+exports.html = html;
+
+// Scripts 
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  .pipe(terser())
+  .pipe(rename("script.min.js"))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+
+// Images
+
+const imageOpt = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.imageOpt = imageOpt;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.copyImages = copyImages; 
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+// Sprites
+
+// Copy 
+
+const copy = () => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+    "!source/img/icons/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+    done();
+}
+
+exports.copy = copy;
+
+// Clean 
+
+const clean = () => {
+  return delete("build");
+};
+
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -39,6 +121,11 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+const reload = (done) => {
+  sync.reload();
+  done();
+}
 // Watcher
 
 const watcher = () => {
